9cf15327b49e4130dc249ed23dea9eb340caa341
a-r-s-h-a-d <muhammedarshad115@gmail.com>
2023-04-10 16:09:46 +0530

    bugs fixed and version 1.0.1 deployed

--- lib/services/functions/sign_in/sign_in.dart
+++ lib/services/functions/sign_in/sign_in.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:smartbuy/main.dart';
+import 'package:smartbuy/pages/tabs/main_page/screen_mainpage.dart';
 
 signIn(BuildContext context, TextEditingController emailController,
     TextEditingController passwordController) async {
@@ -20,6 +20,11 @@ signIn(BuildContext context, TextEditingController emailController,
     final snackbar = SnackBar(content: Text(e.toString()));
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
   }
-  navigatorKey.currentState!.popUntil((route) => route.isFirst);
+  // ignore: use_build_context_synchronously
+  Navigator.of(context).pushAndRemoveUntil(
+    MaterialPageRoute(builder: (context) => ScreenMainPage()),
+    (route) => false,
+  );
+
   // Get.offAll(() => ScreenMainPage);
 }
